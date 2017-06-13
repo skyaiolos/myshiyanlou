718f7f61489199e9d10c279a1af1b2d217c8b5d5
skyaiolos <skyaiolos@aliyun.com>
2017-06-13 18:42:55 +0800

    update for structural pattern

---
+++ designPattern/structuralPattern/compositePattern.py
@@ -0,0 +1,13 @@
+__author__ = "Jianguo Jin (jinjianguosky@hotmail.com)"
+
+#!/usr/bin/python3 
+# -*- coding:utf-8 -*-  
+# Created by Jianguo on 2017/6/13
+"""
+    Description:
+        
+"""
+
+if __name__ == '__main__':
+    pass
+
